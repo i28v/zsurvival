c894f202b0856e8975fe2a8ac797f5a855f134fb
i28v <legomail90520@gmail.com>
2020-02-18 12:54:46 -0700

    Add files via upload

--- src/main.asm
+++ src/main.asm
@@ -21,351 +21,352 @@ section .text
 %define key_s 0x1F73
 %define key_d 0x2064
 
+
+
 %macro print 1
-    push ax
-    push si
-    mov si, %1
-    mov ah, 0x0E
+        push ax
+        push si
+        mov si, %1
+        mov ah, 0x0E
 .L1:
-    lodsb
-    or al, al 
-    jz .end 
-    int 0x10
-    jmp .L1
+        lodsb
+        or al, al 
+        jz .end 
+        int 0x10
+        jmp .L1
 .end:
-    pop si 
-    pop ax
+        pop si 
+        pop ax
 %endmacro
 
 %macro printChar 1
-    push ax
-    mov al, %1
-    mov ah, 0x0E
-    int 0x10
-    pop ax
+        push ax
+        mov al, %1
+        mov ah, 0x0E
+        int 0x10
+        pop ax
 %endmacro
 
 %macro moveCursor 2
-    push ax 
-    push bx 
-    push dx
-    mov dl, %1
-    mov dh, %2
-    xor bh, bh 
-    mov ah, 0x02
-    int 0x10
-    pop dx 
-    pop bx 
-    pop ax 
+        push ax 
+        push bx 
+        push dx
+        mov dl, %1
+        mov dh, %2
+        xor bh, bh 
+        mov ah, 0x02
+        int 0x10
+        pop dx 
+        pop bx 
+        pop ax 
 %endmacro
 
 delay:
-    push ax
-    push bx
-    push cx
-    push dx
-    mov ah, 0x00 
-    int 0x1A
-    add bx, dx 
+        push ax
+        push bx
+        push cx
+        push dx
+        mov ah, 0x00 
+        int 0x1A
+        add bx, dx 
 .tloop:
-    mov ah, 0x00
-    int 0x1A
-    cmp dx, bx 
-    jg .tloop2 
-    jmp .tloop 
+        mov ah, 0x00
+        int 0x1A
+        cmp dx, bx 
+        jg .tloop2 
+        jmp .tloop 
 .tloop2:
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    ret
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
 
 clear:
-    mov ah, 0
-    mov al, 3
-    int 0x10        
-    ret
+        mov ah, 0
+        mov al, 3
+        int 0x10        
+        ret
 
 clearkeyboardbuffer:
-    mov ah, 0x0C
-    mov al, 0
-    int 0x21
-    ret 
+        mov ah, 0x0C
+        mov al, 0
+        int 0x21
+        ret 
 
 _start:
-    call init 
-    jmp mainGameLoop
+        call init 
+        jmp mainGameLoop
 
 _finish:
-    mov ax, 0x4C00
-    int 0x21
+        mov ax, 0x4C00
+        int 0x21
 
 mainGameLoop:
-    call clear
-    call draw
-    xor dx, dx 
-    xor bx, bx
-    call delay
-    call input
-    call update
-    mov al, [gameOver]
-    test al, al 
-    jnz _finish
-    jz mainGameLoop
+        call clear
+        call draw
+        xor dx, dx 
+        xor bx, bx
+        call delay
+        call input
+        call update
+        mov al, [gameOver]
+        test al, al 
+        jnz _finish
+        jz mainGameLoop
 
 init:
-    xor ax, ax 
-    xor bx, bx 
-    xor cx, cx
-    xor dx, dx 
-    xor si, si 
-    xor di, di 
-    mov byte [gameOver], 0
-    mov byte [playerX], 1
-    mov byte [playerY], 10
-    mov byte [playerDirection], still
-    mov byte [isFiringBullet], 0
-    mov cx, 10
-    xor bx, bx
+        xor ax, ax 
+        xor bx, bx 
+        xor cx, cx
+        xor dx, dx 
+        xor si, si 
+        xor di, di 
+        mov byte [gameOver], 0
+        mov byte [playerX], 1
+        mov byte [playerY], 10
+        mov byte [playerDirection], still
+        mov byte [isFiringBullet], 0
+        mov cx, 64
+        xor bx, bx
 .initializeBulletsX:
-    mov byte [bulletsX + bx], 0x50
-    inc bx 
-    loop .initializeBulletsX
-    mov cx, 10 
-    xor bx, bx 
+        mov byte [bulletsX + bx], 0x50
+        inc bx 
+        loop .initializeBulletsX
+        mov cx, 64
+        xor bx, bx 
 .initializeBulletsY:
-    mov byte [bulletsY + bx], 0x50
-    inc bx 
-    loop .initializeBulletsY 
-    ret 
+        mov byte [bulletsY + bx], 0x50
+        inc bx 
+        loop .initializeBulletsY 
+        ret 
 
 draw: 
-    moveCursor [playerX], [playerY]
-    printChar 0x02
-    
-    mov cx, 10
-    xor bx, bx
+        moveCursor [playerX], [playerY]
+        printChar 0x02
+        mov cx, 64
+        xor bx, bx
 .printBullets:
-    mov al, [isIndividualBulletBeingFired + bx]
-    test al, al 
-    jz .skip
-    moveCursor[bulletsX+bx], [bulletsY+bx]
-    printChar '*'
+        mov al, [isIndividualBulletBeingFired + bx]
+        test al, al 
+        jz .skip
+        moveCursor[bulletsX+bx], [bulletsY+bx]
+        printChar '*'
 .skip:
-    inc bx
-    loop .printBullets
-    moveCursor 0, 0
-    ret
+        inc bx
+        loop .printBullets
+        moveCursor 0, 0
+        ret
 
 input:
-    mov ah, 0x01
-    int 0x16
-    cmp ax, upArrow
-    je .up_pressed
-    cmp ax, downArrow
-    je .down_pressed
-    cmp ax, leftArrow
-    je .left_pressed
-    cmp ax, rightArrow
-    je .right_pressed
-    cmp ax, key_w
-    je .w_pressed
-    cmp ax, key_a 
-    je .a_pressed
-    cmp ax, key_s 
-    je .s_pressed
-    cmp ax, key_d 
-    je .d_pressed 
-    jmp .checkForOtherKeys
+        mov ah, 0x01
+        int 0x16
+        cmp ax, upArrow
+        je .up_pressed
+        cmp ax, downArrow
+        je .down_pressed
+        cmp ax, leftArrow
+        je .left_pressed
+        cmp ax, rightArrow
+        je .right_pressed
+        cmp ax, key_w
+        je .w_pressed
+        cmp ax, key_a 
+        je .a_pressed
+        cmp ax, key_s 
+        je .s_pressed
+        cmp ax, key_d 
+        je .d_pressed 
+        jmp .checkForOtherKeys
 .up_pressed:
-    call clearkeyboardbuffer
-    mov byte[playerDirection], up
-    jmp .end
+        call clearkeyboardbuffer
+        mov byte[playerDirection], up
+        jmp .end
 .down_pressed:
-    call clearkeyboardbuffer
-    mov byte[playerDirection], down 
-    jmp .end 
+        call clearkeyboardbuffer
+        mov byte[playerDirection], down 
+        jmp .end 
 .left_pressed:
-    call clearkeyboardbuffer
-    mov byte[playerDirection], left
-    jmp .end
+        call clearkeyboardbuffer
+        mov byte[playerDirection], left
+        jmp .end
 .right_pressed:
-    call clearkeyboardbuffer
-    mov byte[playerDirection], right 
-    jmp .end 
+        call clearkeyboardbuffer
+        mov byte[playerDirection], right 
+        jmp .end 
 .w_pressed:
-    call clearkeyboardbuffer
-    mov byte [isFiringBullet], 1
-    mov byte [bulletFiringDirection], up 
-    jmp .end 
+        call clearkeyboardbuffer
+        mov byte [isFiringBullet], 1
+        mov byte [bulletFiringDirection], up 
+        jmp .end 
 .a_pressed:
-    call clearkeyboardbuffer
-    mov byte [isFiringBullet], 1
-    mov byte [bulletFiringDirection], left
-    jmp .end 
+        call clearkeyboardbuffer
+        mov byte [isFiringBullet], 1
+        mov byte [bulletFiringDirection], left
+        jmp .end 
 .s_pressed:
-    call clearkeyboardbuffer
-    mov byte [isFiringBullet], 1
-    mov byte [bulletFiringDirection], down 
-    jmp .end 
+        call clearkeyboardbuffer
+        mov byte [isFiringBullet], 1
+        mov byte [bulletFiringDirection], down 
+        jmp .end 
 .d_pressed:
-    call clearkeyboardbuffer
-    mov byte[isFiringBullet], 1
-    mov byte [bulletFiringDirection], right
-    jmp .end
+        call clearkeyboardbuffer
+        mov byte[isFiringBullet], 1
+        mov byte [bulletFiringDirection], right
+        jmp .end
 .checkForOtherKeys:
-    mov byte [playerDirection], still
-    test ax, ax 
-    jz .end 
-    call clearkeyboardbuffer
+        mov byte [playerDirection], still
+        test ax, ax 
+        jz .end 
+        call clearkeyboardbuffer
 .end:
-    ret 
+        ret 
 
 update:
-    nop
+        nop
 .movePlayer:
-    mov al, [playerDirection]
-    cmp al, up 
-    je .moveUp
-    cmp al, down 
-    je .moveDown
-    cmp al, left 
-    je .moveLeft
-    cmp al, right 
-    je .moveRight
-    jmp .finishMovePlayer
+        mov al, [playerDirection]
+        cmp al, up 
+        je .moveUp
+        cmp al, down 
+        je .moveDown
+        cmp al, left 
+        je .moveLeft
+        cmp al, right 
+        je .moveRight
+        jmp .finishMovePlayer
 .moveUp:
-    dec byte[playerY]
-    jmp .finishMovePlayer
+        dec byte[playerY]
+        jmp .finishMovePlayer
 .moveDown:
-    inc byte[playerY]
-    jmp .finishMovePlayer
+        inc byte[playerY]
+        jmp .finishMovePlayer
 .moveLeft:
-    dec byte[playerX]
-    jmp .finishMovePlayer
+        dec byte[playerX]
+        jmp .finishMovePlayer
 .moveRight:
-    inc byte[playerX]
+        inc byte[playerX]
 .finishMovePlayer:
-    nop
+        nop
 .checkForBullet:
-    mov al, [isFiringBullet]
-    test al, al 
-    jnz .fireBullet
+        mov al, [isFiringBullet]
+        test al, al 
+        jnz .fireBullet
     jmp .finishFireBullet
 .fireBullet:
-    mov al, [numberOfBulletsBeingFired]
-    cmp al, 10
-    jle .thereAreLessThanElevenBullets
-    jmp .finishFireBullet
-.thereAreLessThanElevenBullets:
-    test al, al 
-    jz .skipIncrementingBulletCount
-    inc byte [numberOfBulletsBeingFired]
+        mov al, [numberOfBulletsBeingFired]
+        cmp al, 64
+        jle .thereAreLessThanMaxBullets
+        jmp .finishFireBullet
+.thereAreLessThanMaxBullets:
+        test al, al 
+        jz .skipIncrementingBulletCount
+        inc byte [numberOfBulletsBeingFired]
 .skipIncrementingBulletCount:
-    mov bx, [numberOfBulletsBeingFired]
-    mov byte [isIndividualBulletBeingFired + bx], 1
-    mov al, [bulletFiringDirection]
-    cmp al, up
-    je .fire_bullet_up
-    cmp al, down 
-    je .fire_bullet_down
-    cmp al, left 
-    je .fire_bullet_left 
-    cmp al, right 
-    je .fire_bullet_right 
-    jmp .finishFireBullet
+        mov bx, [numberOfBulletsBeingFired]
+        mov byte [isIndividualBulletBeingFired + bx], 1
+        mov al, [bulletFiringDirection]
+        cmp al, up
+        je .fire_bullet_up
+        cmp al, down 
+        je .fire_bullet_down
+        cmp al, left 
+        je .fire_bullet_left 
+        cmp al, right 
+        je .fire_bullet_right 
+        jmp .finishFireBullet
 .fire_bullet_up:
-    mov byte [individualBulletDirections + bx], up
-    mov al, [playerX]
-    mov byte [bulletsX + bx], al
-    mov al, [playerY]
-    mov byte [bulletsY + bx], al
-    dec byte [bulletsY + bx]
-    jmp .finishFireBullet 
+        mov byte [individualBulletDirections + bx], up
+        mov al, [playerX]
+        mov byte [bulletsX + bx], al
+        mov al, [playerY]
+        mov byte [bulletsY + bx], al
+        dec byte [bulletsY + bx]
+        jmp .finishFireBullet 
 .fire_bullet_down:
-    mov byte [individualBulletDirections + bx], down
-    mov al, [playerX]
-    mov byte [bulletsX + bx], al
-    mov al, [playerY]
-    mov byte [bulletsY + bx], al
-    inc byte [bulletsY + bx]
-    jmp .finishFireBullet
+        mov byte [individualBulletDirections + bx], down
+        mov al, [playerX]
+        mov byte [bulletsX + bx], al
+        mov al, [playerY]
+        mov byte [bulletsY + bx], al
+        inc byte [bulletsY + bx]
+        jmp .finishFireBullet
 .fire_bullet_left:
-    mov byte [individualBulletDirections + bx], left 
-    mov al, [playerX]
-    mov byte [bulletsX + bx], al 
-    mov al, [playerY]
-    mov byte [bulletsY + bx], al 
-    dec byte [bulletsX + bx]
-    jmp .finishFireBullet
+        mov byte [individualBulletDirections + bx], left 
+        mov al, [playerX]
+        mov byte [bulletsX + bx], al 
+        mov al, [playerY]
+        mov byte [bulletsY + bx], al 
+        dec byte [bulletsX + bx]
+        jmp .finishFireBullet
 .fire_bullet_right:
-    mov byte [individualBulletDirections + bx], right
-    mov al, [playerX]
-    mov byte [bulletsX + bx], al 
-    mov al, [playerY]
-    mov byte [bulletsY + bx], al 
-    inc byte [bulletsX + bx]
+        mov byte [individualBulletDirections + bx], right
+        mov al, [playerX]
+        mov byte [bulletsX + bx], al 
+        mov al, [playerY]
+        mov byte [bulletsY + bx], al 
+        inc byte [bulletsX + bx]
 .finishFireBullet:
-    mov byte [isFiringBullet], 0
-    mov al, [numberOfBulletsBeingFired]
-    test al, al 
-    jnz .firingBulletComplete
-    inc byte [numberOfBulletsBeingFired]
+        mov byte [isFiringBullet], 0
+        mov al, [numberOfBulletsBeingFired]
+        test al, al 
+        jnz .firingBulletComplete
+        inc byte [numberOfBulletsBeingFired]
 .firingBulletComplete:
-    nop
+        nop
 .moveBullets:
-    mov cx, 10 
-    xor bx, bx 
+        mov cx, 64
+        xor bx, bx 
 .moveBulletsLoop:
-    mov al, [isIndividualBulletBeingFired + bx]
-    test al, al 
-    jz .finishMovingBullet
-    mov al, [individualBulletDirections + bx]
-    cmp al, up 
-    je .moveBulletUp
-    cmp al, down 
-    je .moveBulletDown
-    cmp al, left 
-    je .moveBulletLeft 
-    cmp al, right 
-    je .moveBulletRight
-    jmp .finishMovingBullet
+        mov al, [isIndividualBulletBeingFired + bx]
+        test al, al 
+        jz .finishMovingBullet
+        mov al, [individualBulletDirections + bx]
+        cmp al, up 
+        je .moveBulletUp
+        cmp al, down 
+        je .moveBulletDown
+        cmp al, left 
+        je .moveBulletLeft 
+        cmp al, right 
+        je .moveBulletRight
+        jmp .finishMovingBullet
 .moveBulletUp:
-    dec byte[bulletsY + bx]
-    mov al, [bulletsY + bx]
-    cmp al, 1
-    jg .finishMovingBullet
-    mov byte [isIndividualBulletBeingFired + bx], 0
-    dec byte [numberOfBulletsBeingFired]
-    jmp .finishMovingBullet
+        dec byte[bulletsY + bx]
+        mov al, [bulletsY + bx]
+        cmp al, 0
+        jg .finishMovingBullet
+        mov byte [isIndividualBulletBeingFired + bx], 0
+        dec byte [numberOfBulletsBeingFired]
+        jmp .finishMovingBullet
 .moveBulletDown:
-    inc byte[bulletsY + bx]
-    mov al, [bulletsY + bx]
-    cmp al, 24 
-    jl .finishMovingBullet
-    mov byte [isIndividualBulletBeingFired + bx], 0 
-    dec byte [numberOfBulletsBeingFired]
-    jmp .finishMovingBullet
+        inc byte[bulletsY + bx]
+        mov al, [bulletsY + bx]
+        cmp al, 25 
+        jl .finishMovingBullet
+        mov byte [isIndividualBulletBeingFired + bx], 0 
+        dec byte [numberOfBulletsBeingFired]
+        jmp .finishMovingBullet
 .moveBulletLeft:
-    dec byte[bulletsX + bx]
-    mov al, [bulletsX + bx]
-    cmp al, 1
-    jg .finishMovingBullet
-    mov byte [isIndividualBulletBeingFired + bx], 0
-    dec byte [numberOfBulletsBeingFired]
-    jmp .finishMovingBullet
+        dec byte[bulletsX + bx]
+        mov al, [bulletsX + bx]
+        cmp al, 0
+        jg .finishMovingBullet
+        mov byte [isIndividualBulletBeingFired + bx], 0
+        dec byte [numberOfBulletsBeingFired]
+        jmp .finishMovingBullet
 .moveBulletRight:
-    inc byte[bulletsX + bx]    
-    mov al, [bulletsX + bx]
-    cmp al, 79
-    jl .finishMovingBullet
-    mov byte [isIndividualBulletBeingFired + bx], 0
-    dec byte [numberOfBulletsBeingFired]
+        inc byte[bulletsX + bx]    
+        mov al, [bulletsX + bx]
+        cmp al, 80
+        jl .finishMovingBullet
+        mov byte [isIndividualBulletBeingFired + bx], 0
+        dec byte [numberOfBulletsBeingFired]
 .finishMovingBullet:
-    inc bx 
-    loop .moveBulletsLoop    
+        inc bx 
+        loop .moveBulletsLoop    
 .end:
-    ret
+         ret
 section .data
 
 section .bss
@@ -380,11 +381,11 @@ isFiringBullet: resb 1
 
 bulletFiringDirection: resb 1
 
-bulletsX: resb 10
-bulletsY: resb 10
+bulletsX: resb 64
+bulletsY: resb 64
 
-isIndividualBulletBeingFired: resb 10
-individualBulletDirections: resb 10
+isIndividualBulletBeingFired: resb 64
+individualBulletDirections: resb 64
 
 numberOfBulletsBeingFired: resb 1
 
